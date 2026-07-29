c3a692291eca9054d4dc286c795d3a12991c4bbb
Maanak-Lamba <maanak.lamba@dcmail.ca>
2026-07-29 18:07:50 -0400

    Updated collisiondetect() method

--- assignment 4/part 4/mian-finished.js
+++ assignment 4/part 4/mian-finished.js
@@ -103,3 +103,21 @@ function loop() {
 }
 
 loop();
+
+class Ball {
+  // …
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  // …
+}
