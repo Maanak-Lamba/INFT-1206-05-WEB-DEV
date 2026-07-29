51de721983e5278aec6471d7e39664a92359e807
Maanak-Lamba <maanak.lamba@dcmail.ca>
2026-07-28 20:44:05 -0400

    defined class ball and added update function

--- assignment 4/part 3/main.js
+++ assignment 4/part 3/main.js
@@ -18,3 +18,34 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+class Ball {
+  constructor(x, y, velX, velY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.size = size;
+    this.color = color;
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
