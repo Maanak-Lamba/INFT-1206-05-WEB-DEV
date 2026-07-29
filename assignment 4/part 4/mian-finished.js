705287473d2809b64345de389f85c44c629f81f8
Maanak-Lamba <maanak.lamba@dcmail.ca>
2026-07-29 18:08:49 -0400

    Added evil circle

--- assignment 4/part 4/mian-finished.js
+++ assignment 4/part 4/mian-finished.js
@@ -121,3 +121,20 @@ class Ball {
   }
   // …
 }
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
